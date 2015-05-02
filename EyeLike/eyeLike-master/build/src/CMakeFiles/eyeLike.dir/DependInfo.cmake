ced3ffb767e023732f9491f24a95b0702e78a893
Maxwell Johnson <johnsoms@stolaf.edu>
2015-05-01 20:03:43 -0500

    Adding build of eyelike

---
+++ EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/src/findEyeCenter.cpp" "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/src/findEyeCorner.cpp" "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/src/helpers.cpp" "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/src/main.cpp" "/home/users/johnsoms/SD/proj/mind/EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/opencv"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ EyeLike/eyeLike-master/build/src/CMakeFiles/eyeLike.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/eyeLike.dir/main.cpp.o"
+  "CMakeFiles/eyeLike.dir/findEyeCenter.cpp.o"
+  "CMakeFiles/eyeLike.dir/findEyeCorner.cpp.o"
+  "CMakeFiles/eyeLike.dir/helpers.cpp.o"
+  "../bin/eyeLike.pdb"
+  "../bin/eyeLike"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eyeLike.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
